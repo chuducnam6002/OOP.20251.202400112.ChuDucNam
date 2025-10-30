4d63ed5c7cf8d62b39c9bd63a272cfb5b39fa965
chuducnam6002 <chuducnam2k6@gmail.com>
2025-10-30 14:47:26 +0700

    Upload Cart.java

--- src/AimsProject/Cart.java
+++ src/AimsProject/Cart.java
@@ -98,6 +98,26 @@ public class Cart {
         
         System.out.println("***************************************************");
     }
+    public void searchById(int id) {
+        boolean found = false;
+        
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].getId() == id) {
+                System.out.println(itemsOrdered[i].toString());
+                found = true;
+                break; 
+            }
+        }
+    }
 
-
+    public void searchByTitle(String title) {
+        boolean found = false;
+        
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].isMatch(title)) {
+                System.out.println(itemsOrdered[i].toString());
+                found = true;
+            }
+        }
+    }
 }
